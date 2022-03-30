9b9fc86fad7a84875ca6790627259ee6a12d493e
ags1773 <amogh@quintype.com>
2022-03-31 00:26:49 +0530

    get ahead asset requests

--- src/app.js
+++ src/app.js
@@ -4,26 +4,19 @@ const fs = require("fs").promises;
 
 async function main() {
   const pathToOutputFile = path.join(__dirname, "../io/output.csv");
-  const publisherData = await getJsonData(
-    path.join(__dirname, "../io/publisher.csv")
+  // const publisherData = await getJsonData(
+  //   path.join(__dirname, "../io/publisher.csv")
+  // );
+  // const publisherDomains = publisherData.map((data) => data.domain_url);
+  const assetData = await getJsonData(
+    path.join(__dirname, "../io/ahead-asset-data.csv")
   );
-  const publisherDomains = publisherData.map((data) => data.domain_url);
-  const assetData = (
-    await getJsonData(path.join(__dirname, "../io/ahead-asset-data.csv"))
-  )
-    .filter((data) => publisherDomains.includes(data.referer_domain))
-    .map((data) => {
-      const { name: publisherName } = publisherData.find(
-        (pubData) => pubData.domain_url === data.referer_domain
-      );
-      return { publisher_name: publisherName, ...data };
-    })
-    .reduce((acc, row) => {
-      acc += `"${row.publisher_name}","${row.referer_domain}","${row.total_requests}","${row.total_bytes}","${row.hit_count}","${row.date}"\n`;
-      return acc;
-    }, `"publisher_name","referer_domain","total_requests","total_bytes","hit_count","date"\n`);
-
-  await fs.writeFile(pathToOutputFile, assetData);
+  const pubNameArr = assetData.reduce((acc, row) => {
+    acc.push(`'${row.publisher_name}'`);
+    return acc;
+  }, []);
+  const uniq = [...new Set(pubNameArr)];
+  await fs.writeFile(pathToOutputFile, uniq);
 }
 
 main();
