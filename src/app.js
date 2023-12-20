ad587c0051010ddcbec9ac1cac7b77245c47fd56
Amogh Sahasrabhojanee <amogh1773@gmail.com>
2023-12-20 13:47:27 +0530

    redirect urls JSON

--- src/app.js
+++ src/app.js
@@ -1,22 +1,28 @@
 const csv = require("csvtojson");
 const path = require("path");
 const fs = require("fs").promises;
-const inputFilePath = path.join(__dirname, "../io/test-redirects.csv");
-// const inputFilePath = path.join(__dirname, "../io/healthday-40k-redirects.csv");
-const outputFilePath = path.join(__dirname, "../io/output.csv");
+// const inputFilePath = path.join(__dirname, "../io/test-input.csv");
+const inputFilePath = path.join(__dirname, "../io/healthday-40k-redirects.csv");
+const outputFilePath = path.join(__dirname, "../io/output.json");
 
-const healthdayEnBaseUrl = "https://www.healthday.com";
+// const healthdayEnBaseUrl = "https://www.healthday.com";
 
 async function main() {
   const redirects = await getJsonData(inputFilePath);
-  const finalData = redirects.reduce((accum, redirect) => {
-    const url = new URL(redirect.Slug);
-    const srcUrl = `${healthdayEnBaseUrl}${url.pathname}`;
-    const destUrl = `${url.origin}${url.pathname}`;
-    accum += `${srcUrl},${destUrl}\n`;
-    return accum;
-  }, `source,destination\n`);
-  await fs.writeFile(outputFilePath, finalData);
+  // console.log("** redirects >> ", redirects);
+
+  const jsonOutput = redirects.reduce(
+    (acc, redirect) => {
+      const sourceUrl = redirect.source;
+      const destinationUrl = redirect.destination;
+      acc.redirectUrls.push({ sourceUrl, destinationUrl, statusCode: `NumberInt(301)` });
+      return acc;
+    },
+    {
+      redirectUrls: [],
+    }
+  );
+  await fs.writeFile(outputFilePath, JSON.stringify(jsonOutput))
 }
 
 main();
