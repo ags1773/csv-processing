bc7fe1d17a934719f38c4a10667ffa43e9aaa5e0
Amogh Sahasrabhojanee <amogh1773@gmail.com>
2024-03-08 15:35:48 +0530

    merge branch

--- src/app.js
+++ src/app.js
@@ -1,22 +1,45 @@
 const csv = require("csvtojson");
 const path = require("path");
 const fs = require("fs").promises;
-// const inputFilePath = path.join(__dirname, "../io/test-redirects.csv");
-const inputFilePath = path.join(__dirname, "../io/healthday-40k-redirects.csv");
-const outputFilePath = path.join(__dirname, "../io/output.csv");
+const inputFilePath = path.join(
+  __dirname,
+  "../io/Untitled spreadsheet - Sheet1.csv"
+);
+const _ = require("lodash");
 
-const healthdayEnBaseUrl = "https://www.healthday.com";
+const outputFilePath = path.join(__dirname, "../io/redirects.csv");
 
 async function main() {
   const redirects = await getJsonData(inputFilePath);
-  const finalData = redirects.reduce((accum, redirect) => {
-    const url = new URL(redirect.Slug);
-    const srcUrl = `${healthdayEnBaseUrl}${url.pathname}`;
-    const destUrl = `${url.origin}${url.pathname}`;
-    accum += `${srcUrl},${destUrl}\n`;
-    return accum;
-  }, `source,destination\n`);
-  await fs.writeFile(outputFilePath, finalData);
+  console.log(`[INFO] Processing ${redirects.length} entries`);
+  const sourceUrls = [];
+  const duplicates = [];
+  const output = redirects.reduce((acc, redirect) => {
+    if (
+      redirect.sourceUrl.startsWith("https://resident.com/") &&
+      redirect.destinationUrl === "/"
+    ) {
+      const sourceUrl = new URL(redirect.sourceUrl);
+      if (sourceUrls.includes(sourceUrl.pathname)) {
+        duplicates.push(sourceUrl.pathname);
+        // throw new Error(`Duplicate source URL ${sourceUrl.pathname}`);
+      } else sourceUrls.push(sourceUrl.pathname);
+      acc += `${
+        decodeURIComponent(sourceUrl.pathname).endsWith("/")
+          ? decodeURIComponent(sourceUrl.pathname).slice(0, -1)
+          : decodeURIComponent(sourceUrl.pathname)
+      },/,301,5548,resident\n`;
+    } else {
+      throw new Error(`*** Problem in >> ${redirect}`);
+    }
+    return acc;
+  }, "sourceUrl,destinationUrl,statusCode,boldPublisher_id,domainKey\n");
+  if (duplicates.length) {
+    console.error("Duplicate entries found, aborting");
+    console.log(duplicates);
+    return;
+  }
+  await fs.writeFile(outputFilePath, output);
 }
 
 main();
